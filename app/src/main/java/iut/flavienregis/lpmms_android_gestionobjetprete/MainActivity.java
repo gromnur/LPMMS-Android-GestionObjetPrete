db75aabe1eaaa383fa548fb5dc7c837d8644c47a
Flavien Regis <flavienregis@gmail.com>
2019-01-14 13:40:21 +0100

    initial commit

---
+++ app/src/main/java/iut/flavienregis/lpmms_android_gestionobjetprete/MainActivity.java
@@ -0,0 +1,13 @@
+package iut.flavienregis.lpmms_android_gestionobjetprete;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
