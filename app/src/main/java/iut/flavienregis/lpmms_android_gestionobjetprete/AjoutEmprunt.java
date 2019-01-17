8cff07900aca09025c5bbb6a753582d73621c7f4
Thomas Gossmann <thomas.gossmann12@gmail.com>
2019-01-17 09:17:57 +0100

    ajout onClic bouton

--- app/src/main/java/iut/flavienregis/lpmms_android_gestionobjetprete/AjoutEmprunt.java
+++ app/src/main/java/iut/flavienregis/lpmms_android_gestionobjetprete/AjoutEmprunt.java
@@ -8,6 +8,6 @@ public class AjoutEmprunt extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ajout_layout);
+        setContentView(R.layout.ajout_pret);
     }
 }
