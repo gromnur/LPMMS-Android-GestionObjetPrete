cd00596d2492b5a55b5bed5e8457aa78375f5eee
Thomas Gossmann <thomas.gossmann12@gmail.com>
2019-01-18 12:19:29 +0100

    Card View + delete

---
+++ app/src/main/java/iut/flavienregis/lpmms_android_gestionobjetprete/Item.java
@@ -0,0 +1,52 @@
+package iut.flavienregis.lpmms_android_gestionobjetprete;
+
+public class Item {
+    int id;
+    String name;
+    String description;
+    String date;
+    String thumbnail;
+
+    public Item() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
--- app/src/main/java/iut/flavienregis/lpmms_android_gestionobjetprete/ListAccueil.java
+++ app/src/main/java/iut/flavienregis/lpmms_android_gestionobjetprete/ListAccueil.java
@@ -1,14 +1,20 @@
 package iut.flavienregis.lpmms_android_gestionobjetprete;
 
 
-import android.app.Activity;
+
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,23 +22,34 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
-public class ListAccueil extends AppCompatActivity {
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class ListAccueil extends AppCompatActivity implements RecyclerItemTouchHelper.RecyclerItemTouchHelperListener {
 
 
     private ArrayList<String> liste;
 
     private ArrayAdapter<String> adaptateur;
 
-    private ListView listePret;
-
+    private static final String TAG = ListAccueil.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private List<Item> cartList;
+    private CartListAdapter mAdapter;
+    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +59,27 @@ public class ListAccueil extends AppCompatActivity {
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        listePret = findViewById(R.id.liste_pret);
+        recyclerView = findViewById(R.id.recycler_view);
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
+        cartList = new ArrayList<>();
+        mAdapter = new CartListAdapter(this, cartList);
 
-        liste = new ArrayList<>();
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(mAdapter);
 
-        adaptateur = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, liste);
+        // adding item touch helper
+        // only ItemTouchHelper.LEFT added to detect Right to Left swipe
+        // if you want both Right -> Left and Left -> Right
+        // add pass ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT as param
+        ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new RecyclerItemTouchHelper(0, ItemTouchHelper.LEFT, this);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(recyclerView);
 
-        listePret.setAdapter(adaptateur);
 
-        registerForContextMenu(listePret);
+        // making http call and fetching menu json
+        prepareCart();
     }
 
     @Override
@@ -101,5 +130,73 @@ public class ListAccueil extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * callback when recycler view is swiped
+     * item will be removed on swiped
+     * undo option will be provided in snackbar to restore the item
+     */
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position) {
+        if (viewHolder instanceof CartListAdapter.MyViewHolder) {
+            // get the removed item name to display it in snack bar
+            String name = cartList.get(viewHolder.getAdapterPosition()).getName();
+
+            // backup of removed item for undo purpose
+            final Item deletedItem = cartList.get(viewHolder.getAdapterPosition());
+            final int deletedIndex = viewHolder.getAdapterPosition();
+
+            // remove the item from recycler view
+            mAdapter.removeItem(viewHolder.getAdapterPosition());
+
+            // showing snack bar with Undo option
+            Snackbar snackbar = Snackbar
+                    .make(coordinatorLayout, name + " removed from cart!", Snackbar.LENGTH_LONG);
+            snackbar.setAction("UNDO", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    // undo is selected, restore the deleted item
+                    mAdapter.restoreItem(deletedItem, deletedIndex);
+                }
+            });
+            snackbar.setActionTextColor(Color.YELLOW);
+            snackbar.show();
+        }
+    }
+
+    /**
+     * method make volley network call and parses json
+     */
+    private void prepareCart() {
+        JsonArrayRequest request = new JsonArrayRequest(URL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        if (response == null) {
+                            Toast.makeText(getApplicationContext(), "Couldn't fetch the menu! Pleas try again.", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+
+                        List<Item> items = new Gson().fromJson(response.toString(), new TypeToken<List<Item>>() {
+                        }.getType());
+
+                        // adding items to cart list
+                        cartList.clear();
+                        cartList.addAll(items);
+
+                        // refreshing recycler view
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // error in getting json
+                Log.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MyApplication.getInstance().addToRequestQueue(request);
+    }
 
 }
